440e72083ef5d2b154f02d35fe28395292946a08
App Academy Student <student@appacademy.io>
2016-10-04 10:58:34 -0700

    Created cats table

---
+++ db/migrate/20161004175427_create_cats_table.rb
@@ -0,0 +1,11 @@
+class CreateCatsTable < ActiveRecord::Migration
+  def change
+    create_table :cats do |t|
+      t.date :birth_date, null: false
+      t.string :color, null: false
+      t.string :name, null: false
+      t.string :sex
+      t.text :description
+    end
+  end
+end
